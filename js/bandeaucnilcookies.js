fe01ffff236e0036bff170a3f0de71eedce6fe3b
Hadrien <hadrienboyer@gmail.com>
2021-12-10 22:48:16 +0100

    Update bandeaucnilcookies.js
    small updates

--- js/bandeaucnilcookies.js
+++ js/bandeaucnilcookies.js
@@ -2,14 +2,17 @@
  * Auteur: Hadrien Boyer
  */
 
-function checkCNILBandeau(){
-    if ( document.cookie.indexOf("cnilcookie28393x") < 0 ){
-    document.getElementById("bandeauCNIL").style.display = "block";
+function checkCNILBandeau() {
+    if (document.cookie.indexOf("cnilcookie28393x") < 0) {
+    	document.getElementById("bandeauCNIL").style.display = "block";
     }
 }
 
-function acceptCookies(){
+function acceptCookies() {
 	document.cookie = "gyz$xx=cnilcookie28393x; expires=" + (new Date(2021,0,1)).toUTCString();
-	document.getElementById('bandeauCNIL').style.display='none';document.getElementById('confirmBtn').style.display='none';return false;
+	document.getElementById('bandeauCNIL').style.display='none';
+	document.getElementById('confirmBtn').style.display='none';
+	return false;
 }
+
 window.onload = checkCNILBandeau;
